a1cdc14f8927727f63a44ac3a0e3c2b93d0891ea
deepakjadiwal <jadiwaldeepak12@gmail.com>
2025-08-20 15:24:03 +0530

    first commit

---
+++ src/components/layout/shared/Logo.jsx
@@ -0,0 +1,80 @@
+'use client'
+
+// React Imports
+import { useEffect, useRef } from 'react'
+
+// Third-party Imports
+import styled from '@emotion/styled'
+
+// Component Imports
+import VuexyLogo from '@core/svg/Logo'
+import NewLogo from "../vertical/logo.svg"
+
+// Config Imports
+import themeConfig from '@configs/themeConfig'
+
+// Hook Imports
+import useVerticalNav from '@menu/hooks/useVerticalNav'
+import { useSettings } from '@core/hooks/useSettings'
+import Image from 'next/image'
+
+const LogoText = styled.span`
+  color: ${({ color }) => color ?? 'var(--mui-palette-text-primary)'};
+  font-size: 1.375rem;
+  line-height: 1.09091;
+  font-weight: 700;
+  letter-spacing: 0.25px;
+  transition: ${({ transitionDuration }) =>
+    `margin-inline-start ${transitionDuration}ms ease-in-out, opacity ${transitionDuration}ms ease-in-out`};
+
+  ${({ isHovered, isCollapsed, isBreakpointReached }) =>
+    !isBreakpointReached && isCollapsed && !isHovered
+      ? 'opacity: 0; margin-inline-start: 0;'
+      : 'opacity: 1; margin-inline-start: 12px;'}
+`
+
+const Logo = ({ color }) => {
+  // Refs
+  const logoTextRef = useRef(null)
+
+  // Hooks
+  const { isHovered, transitionDuration, isBreakpointReached } = useVerticalNav()
+  const { settings } = useSettings()
+
+  // Vars
+  const { layout } = settings
+
+  useEffect(() => {
+    if (layout !== 'collapsed') {
+      return
+    }
+
+    if (logoTextRef && logoTextRef.current) {
+      if (!isBreakpointReached && layout === 'collapsed' && !isHovered) {
+        logoTextRef.current?.classList.add('hidden')
+      } else {
+        logoTextRef.current.classList.remove('hidden')
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isHovered, layout, isBreakpointReached])
+
+  return (
+    <div className='flex items-center'>
+       <Image src={NewLogo} alt="New Logo" height={'auto'} width={120} className='w-full' />
+
+      {/* <LogoText
+        color={color}
+        ref={logoTextRef}
+        isHovered={isHovered}
+        isCollapsed={layout === 'collapsed'}
+        transitionDuration={transitionDuration}
+        isBreakpointReached={isBreakpointReached}
+      >
+        {themeConfig.templateName}
+      </LogoText> */}
+    </div>
+  )
+}
+
+export default Logo
